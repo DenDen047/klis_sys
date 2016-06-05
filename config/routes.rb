618778af47387241e581ebd920497e6da1b4d080
denden047 <naoyashop@yahoo.co.jp>
2016-06-05 07:41:38 +0000

    add user list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:index, :show]
 
   root to: 'registrations#new'
 
